3b7fc55d1b3eebc49a240ecec228e9bbae69fb99
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-11 14:44:46 +0530

    display image and call api to detect images

--- script.js
+++ script.js
@@ -7,7 +7,22 @@ Promise.all([
 ]).then(start)
 
 function start(){
+    const container = document.createElement('div')
+    container.style.position = 'relative'
+    document.body.append(container)
+
     document.body.append('Loaded')
+    imageupload.addEventListener('change', async() => {
+        const image = await faceapi.bufferToImage(imageupload.files[0])
+        document.body.append(image)
+        const canvas = faceapi.createCanvasFromMedia(image)
+        document.body.append(canvas)
+        const displaySize = {width: image.width, height: image.height}
+        faceapi.matchDimensions(canvas, displaySize)
+        const detections = await facepi.detectAllFaces(image)
+        .withFaceLandmarks().withFaceDescriptors()
+        const resizedDetetctions = faceapi.resizeResults(detections, displaySize)
+    })
 }
 
 
